aa9d77b270fac3b8698f5c626467861d42fb9c04
Alejandro Sanchez <alejandrogsanchez18@gmail.com>
2020-05-26 15:12:23 -0300

    commit

--- resources/js/components/DefaultField.vue
+++ resources/js/components/DefaultField.vue
@@ -1,8 +1,8 @@
 <template>
     <div :class="elementSize">
         <field-wrapper :stacked="field.stacked">
-            <div class="px-8" :class="field.stacked ? 'pt-6 w-full' : 'py-6 w-1/5'">
-                <slot>
+            <div class="px-10" :class="field.stacked ? 'pt-4 w-full' : 'py-6 w-1/5'">
+                <slot class="">
                     <form-label
                             :label-for="field.attribute"
                             :class="{ 'mb-2': showHelpText && field.helpText }"
@@ -12,10 +12,14 @@
                         <span v-if="field.required" class="text-danger text-sm">{{
             __('*')
           }}</span>
+          <span v-if="!field.required" class="text-sm">{{
+            __('(Optional)')
+          }}</span>
+            <span v-if="showHelpText" style="color: text-80" v-tooltip="field.helpText"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="15px" width="15px" role="img" aria-hidden="true"><path d="M15.07,11.25L14.17,12.17C13.45,12.89 13,13.5 13,15H11V14.5C11,13.39 11.45,12.39 12.17,11.67L13.41,10.41C13.78,10.05 14,9.55 14,9C14,7.89 13.1,7 12,7A2,2 0 0,0 10,9H8A4,4 0 0,1 12,5A4,4 0 0,1 16,9C16,9.88 15.64,10.67 15.07,11.25M13,19H11V17H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z"></path></svg></span>
                     </form-label>
                 </slot>
             </div>
-            <div class="py-6 px-8" :class="fieldClasses">
+            <div :class="fieldClasses">
                 <slot name="field"/>
 
                 <help-text
@@ -25,9 +29,7 @@
                     {{ firstError }}
                 </help-text>
 
-                <help-text class="help-text mt-2" v-if="showHelpText">
-                    {{ field.helpText }}
-                </help-text>
+
             </div>
         </field-wrapper>
     </div>
@@ -35,6 +37,7 @@
 
 <script>
     import {HandlesValidationErrors, Errors} from 'laravel-nova'
+    import VTooltip from 'v-tooltip'
 
     export default {
         mixins: [HandlesValidationErrors],
@@ -45,6 +48,10 @@
             showHelpText: {type: Boolean, default: true},
             showErrors: {type: Boolean, default: true},
             fullWidthContent: {type: Boolean, default: false},
+        },
+
+        components: {
+            VTooltip,
         },
 
         mounted() {
@@ -80,11 +87,11 @@
             fieldClasses() {
                 return this.fullWidthContent
                     ? this.field.stacked
-                        ? 'w-full'
-                        : 'w-4/5'
+                        ? 'w-full pt-2 pb-8 px-10'
+                        : 'w-4/5 py-6'
                     : this.hasSize
-                        ? 'w-full'
-                        : 'w-1/2'
+                        ? 'w-full pt-2 pb-8 px-10'
+                        : 'w-1/2 py-6'
             },
 
 
@@ -111,3 +118,115 @@
         },
     }
 </script>
+<style>
+.tooltip {
+  display: block !important;
+  z-index: 10000;
+  background: black !important;
+  padding: 0px;
+}
+
+.tooltip .tooltip-inner {
+  background: black;
+  color: white;
+  border-radius: 16px;
+  padding: 5px 10px 4px;
+
+}
+
+.tooltip .tooltip-arrow {
+  width: 0;
+  height: 0;
+  border-style: solid;
+  position: absolute;
+  margin: 5px;
+  border-color: black;
+  z-index: 1;
+}
+
+.tooltip[x-placement^="top"] {
+  margin-bottom: 5px;
+}
+
+.tooltip[x-placement^="top"] .tooltip-arrow {
+  border-width: 5px 5px 0 5px;
+  border-left-color: transparent !important;
+  border-right-color: transparent !important;
+  border-bottom-color: transparent !important;
+  bottom: -5px;
+  left: calc(50% - 5px);
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.tooltip[x-placement^="bottom"] {
+  margin-top: 5px;
+}
+
+.tooltip[x-placement^="bottom"] .tooltip-arrow {
+  border-width: 0 5px 5px 5px;
+  border-left-color: transparent !important;
+  border-right-color: transparent !important;
+  border-top-color: transparent !important;
+  top: -5px;
+  left: calc(50% - 5px);
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+.tooltip[x-placement^="right"] {
+  margin-left: 5px;
+}
+
+.tooltip[x-placement^="right"] .tooltip-arrow {
+  border-width: 5px 5px 5px 0;
+  border-left-color: transparent !important;
+  border-top-color: transparent !important;
+  border-bottom-color: transparent !important;
+  left: -5px;
+  top: calc(50% - 5px);
+  margin-left: 0;
+  margin-right: 0;
+}
+
+.tooltip[x-placement^="left"] {
+  margin-right: 5px;
+}
+
+.tooltip[x-placement^="left"] .tooltip-arrow {
+  border-width: 5px 0 5px 5px;
+  border-top-color: transparent !important;
+  border-right-color: transparent !important;
+  border-bottom-color: transparent !important;
+  right: -5px;
+  top: calc(50% - 5px);
+  margin-left: 0;
+  margin-right: 0;
+}
+
+.tooltip.popover .popover-inner {
+  background: #f9f9f9;
+  color: black;
+  padding: 24px;
+  border-radius: 5px;
+  box-shadow: 0 5px 30px rgba(black, .1);
+}
+
+.tooltip.popover .popover-arrow {
+  border-color: #f9f9f9;
+}
+
+.tooltip[aria-hidden='true'] {
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity .15s, visibility .15s;
+}
+
+.tooltip[aria-hidden='false'] {
+  visibility: visible;
+  opacity: 1;
+  transition: opacity .15s;
+}
+
+
+</style>
