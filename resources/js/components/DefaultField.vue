7713c4bb00291faa07a3ed5d06c1681cf98a4f17
Alejandro Sanchez <alejandrogsanchez18@gmail.com>
2020-06-08 16:49:33 -0300

    cambios

--- resources/js/components/DefaultField.vue
+++ resources/js/components/DefaultField.vue
@@ -5,28 +5,28 @@
                 <slot class="">
                     <form-label
                             :label-for="field.attribute"
-                            :class="{ 'mb-2': showHelpText && field.helpText }"
+                            :class=""
                     >
                         {{ fieldLabel }}
 
                         <span v-if="field.required" class="text-danger text-sm">{{
             __('*')
           }}</span>
-          <span v-if="!field.required" class="text-sm">{{
+         <!--  <span v-if="!field.required" class="text-sm">{{
             __('(Optional)')
-          }}</span>
-            <span v-if="showHelpText" style="color: text-80" v-tooltip="field.helpText"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="15px" width="15px" role="img" aria-hidden="true"><path d="M15.07,11.25L14.17,12.17C13.45,12.89 13,13.5 13,15H11V14.5C11,13.39 11.45,12.39 12.17,11.67L13.41,10.41C13.78,10.05 14,9.55 14,9C14,7.89 13.1,7 12,7A2,2 0 0,0 10,9H8A4,4 0 0,1 12,5A4,4 0 0,1 16,9C16,9.88 15.64,10.67 15.07,11.25M13,19H11V17H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z"></path></svg></span>
+          }}</span> -->
+            <span v-if="field.helpText" style="color: text-80" v-tooltip="field.helpText"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="15px" width="15px" role="img" aria-hidden="true"><path d="M15.07,11.25L14.17,12.17C13.45,12.89 13,13.5 13,15H11V14.5C11,13.39 11.45,12.39 12.17,11.67L13.41,10.41C13.78,10.05 14,9.55 14,9C14,7.89 13.1,7 12,7A2,2 0 0,0 10,9H8A4,4 0 0,1 12,5A4,4 0 0,1 16,9C16,9.88 15.64,10.67 15.07,11.25M13,19H11V17H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z"></path></svg></span>
                     </form-label>
                 </slot>
             </div>
-            <div :class="fieldClasses">
+            <div class="py-6 px-8" :class="fieldClasses">
                 <slot name="field"/>
 
                 <help-text
-                        class="error-text mt-2 text-danger"
-                        v-if="showErrors && hasError"
+                  class="error-text mt-2 text-danger"
+                  v-if="showErrors && hasError"
                 >
-                    {{ firstError }}
+                  {{ firstError }}
                 </help-text>
 
 
@@ -45,7 +45,7 @@
         props: {
             field: {type: Object, required: true},
             fieldName: {type: String},
-            showHelpText: {type: Boolean, default: true},
+            showHelpText: {type: Boolean, default: false},
             showErrors: {type: Boolean, default: true},
             fullWidthContent: {type: Boolean, default: false},
         },
@@ -113,7 +113,7 @@
              * Return if we must to remove or not the bottom border field
              */
             getRemoveBottomBorder() {
-                return this.field.removeBottomBorder || null
+                return true
             }
         },
     }
